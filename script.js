2062135b03b29248833d4101320e30f1df03a39f
Conner Safieddine <41808548+BaalMahan3@users.noreply.github.com>
2024-08-19 11:01:13 -0600

    adjust java date and path

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 setCookie = (cName, cValue, expDays) => {
     let date = new Date();
-    date.setTime(date.getTime() + (expDays * 24 * 60 * 50 * 1000));
+    date.setTime(date.getTime() + (expDays * 24 * 60 * 60 * 1000));
     const expires = "expires=" + date.toUTCString();
     
-    document.cookie = cName + "=" + cValue + "; " + expires + "; path=/, path=/index, path=/osano"
+    document.cookie = cName + "=" + cValue + "; " + expires + "; path=/"
 }
 
 document.querySelector("#cookies-btn").addEventListener("click", () =>{
