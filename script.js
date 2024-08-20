9d552b7fe37654c1b26e98734500b885a399caef
Conner Safieddine <41808548+BaalMahan3@users.noreply.github.com>
2024-08-20 14:42:45 -0600

    "cookie" is now "conwardcookie". give cookie functionality again

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
-/* function setCookie(cName, cValue, expDays) {
+function setCookie(cName, cValue, expDays) {
     let date = new Date();
     date.setTime(date.getTime() + (expDays * 24 * 60 * 60 * 1000));
     const expires = "expires=" + date.toUTCString();
-    
+
     document.cookie = cName + "=" + cValue + "; " + expires + "; path=/";
-} */
+}
 
 // Automatically set the cookie when the script loads
-setCookie("cookie", true, 30);
+setCookie("conwardcookie", true, 30);
 
 // Optional: Hide the cookie banner if you still have one
 /* document.querySelector("#cookies").style.display = "none"; */
