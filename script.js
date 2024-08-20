4c43e03f6642ee4fc53658fa247e9a178570e9d1
Conner Safieddine <41808548+BaalMahan3@users.noreply.github.com>
2024-08-20 13:00:03 -0600

    Run the cookie without consent

--- script.js
+++ script.js
@@ -1,14 +1,15 @@
-setCookie = (cName, cValue, expDays) => {
+function setCookie(cName, cValue, expDays) {
     let date = new Date();
     date.setTime(date.getTime() + (expDays * 24 * 60 * 60 * 1000));
     const expires = "expires=" + date.toUTCString();
     
-    document.cookie = cName + "=" + cValue + "; " + expires + "; path=/"
+    document.cookie = cName + "=" + cValue + "; " + expires + "; path=/";
 }
 
-document.querySelector("#cookies-btn").addEventListener("click", () =>{
-    document.querySelector("#cookies").style.display = "none";
-    setCookie("cookie", true, 30);
+// Automatically set the cookie when the script loads
+setCookie("cookie", true, 30);
 
-    console.log(setCookie);
-})
+// Optional: Hide the cookie banner if you still have one
+document.querySelector("#cookies").style.display = "none";
+
+console.log("Cookie set: ", document.cookie);
