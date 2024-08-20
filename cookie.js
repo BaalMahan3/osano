03fa96cda0aafa4489ba4d9093831fcff8f85975
Conner Safieddine <41808548+BaalMahan3@users.noreply.github.com>
2024-08-20 15:53:49 -0600

    new cookie added

---
+++ cookie.js
@@ -0,0 +1,11 @@
+function setTestCookie() {
+    let date = new Date();
+    date.setTime(date.getTime() + (30 * 24 * 60 * 60 * 1000)); // Cookie expires in 30 days
+    const expires = "expires=" + date.toUTCString();
+
+    document.cookie = "testCookie=TestValue; " + expires + "; path=/";
+    console.log("Test cookie set: ", document.cookie);
+}
+
+// Automatically set the test cookie when the script loads
+setTestCookie();
